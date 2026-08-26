2fe01aee4c95d52897df67b08f83357f117cfd12
Ian Trimble <itrimble@gmail.com>
2026-08-26 16:34:03 -0500

    Add PostHog pageviews for marketing UTMs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import posthog from 'posthog-js'
 import './index.css'
 import App from './App.tsx'
+
+posthog.init('phc_q4H6cMy4vo2kZetsdPjvPNWkJgjhC32MBHnVFj7HTzdf', {
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2026-05-30',
+  disable_session_recording: true,
+  person_profiles: 'identified_only'
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
